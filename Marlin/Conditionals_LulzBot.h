a763b769dd3af6a341a41f01aaf509029981d455
Marcio Teixeira <marcio@alephobjects.com>
2018-11-20 12:44:19 -0700

    Quiver 2.0.0.14

--- Marlin/Conditionals_LulzBot.h
+++ Marlin/Conditionals_LulzBot.h
@@ -51,7 +51,7 @@
  *
  */
 
-#define LULZBOT_FW_VERSION ".13" // Change this with each update
+#define LULZBOT_FW_VERSION ".14" // Change this with each update
 
 #if ( \
     !defined(LULZBOT_Gladiola_Mini) && \

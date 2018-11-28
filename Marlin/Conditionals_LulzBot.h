5446ec9994e226fdf50a9491c390072c88eea668
Marcio Teixeira <marcio@alephobjects.com>
2018-11-28 08:43:48 -0700

    Updated version to 2.0.0.21

--- Marlin/Conditionals_LulzBot.h
+++ Marlin/Conditionals_LulzBot.h
@@ -52,7 +52,7 @@
  *
  */
 
-#define LULZBOT_FW_VERSION ".19" // Change this with each update
+#define LULZBOT_FW_VERSION ".21" // Change this with each update
 
 #if ( \
     !defined(LULZBOT_Gladiola_Mini) && \

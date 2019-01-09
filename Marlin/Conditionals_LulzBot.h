d9d1d9b22fa4a999e5e495aa73f3a25ac09e5297
Marcio Teixeira <marcio@alephobjects.com>
2019-01-09 09:10:29 -0700

    Change version number
    Changed version number to 2.0.0.68

--- Marlin/Conditionals_LulzBot.h
+++ Marlin/Conditionals_LulzBot.h
@@ -52,7 +52,7 @@
  *
  */
 
-#define LULZBOT_FW_VERSION ".67" // Change this with each update
+#define LULZBOT_FW_VERSION ".68" // Change this with each update
 
 #if ( \
     !defined(LULZBOT_Gladiola_Mini) && \

a412a17229ab5ac9ec21730c9d1d2e4393dd2c7f
Marcio Teixeira <marcio@alephobjects.com>
2019-07-12 14:43:13 -0600

    Change TAZ Workhorse LCD name (T8644)

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -870,7 +870,7 @@
 #endif
 
 // Scroll a longer status message into view
-//#define STATUS_MESSAGE_SCROLLING
+#define STATUS_MESSAGE_SCROLLING LULZBOT_STATUS_MESSAGE_SCROLLING
 
 // On the Info Screen, display XY with one decimal place when possible
 //#define LCD_DECIMAL_SMALL_XY

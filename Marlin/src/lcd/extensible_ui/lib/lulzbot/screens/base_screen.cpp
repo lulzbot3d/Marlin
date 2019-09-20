92596fae57e3a2a4e9c0bf56f01c97d20a0228d9
Marcio Teixeira <marcio@alephobjects.com>
2019-09-20 08:26:48 -0600

    Performance optimizations for UI.

--- Marlin/src/lcd/extensible_ui/lib/lulzbot/screens/base_screen.cpp
+++ Marlin/src/lcd/extensible_ui/lib/lulzbot/screens/base_screen.cpp
@@ -36,6 +36,10 @@ void BaseScreen::onEntry() {
   UIScreen::onEntry();
 }
 
+bool BaseScreen::buttonIsPressed(uint8_t tag) {
+  return tag != 0 && EventLoop::get_pressed_tag() == tag;
+}
+
 bool BaseScreen::buttonStyleCallback(CommandProcessor &cmd, uint8_t tag, uint8_t &style, uint16_t &options, bool post) {
   if (post) {
     cmd.colors(normal_btn);
@@ -48,7 +52,7 @@ bool BaseScreen::buttonStyleCallback(CommandProcessor &cmd, uint8_t tag, uint8_t
     }
   #endif
 
-  if (tag != 0 && EventLoop::get_pressed_tag() == tag) {
+  if (buttonIsPressed(tag)) {
     options = OPT_FLAT;
   }
 

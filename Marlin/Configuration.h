lulzbot3d/Marlin

commit 9c98b4b8447bfe5e4b4d0b2782618b09e2603e65

Apply 1 suggestion(s) to 1 file(s)

Marlin/Configuration.h
@@ -10,7 +10,7 @@
 //#define TazDualZ
 
 /************** Uncomment a Tool Head Option From Below *********************/
-#define LULZBOT_UNIVERSAL_TOOLHEAD
+//#define LULZBOT_UNIVERSAL_TOOLHEAD
 //#define TOOLHEAD_SL_SE_HE
 //#define TOOLHEAD_HS_HSPLUS
 //#define TOOLHEAD_H175

commit 03025474a57a4b03e5f6ae45c812ef609ad81440

Fix universal toolhead not being defined
FYI: A toolhead must be defined in order for the build to succeed.

Caused by 9c98b4b8447bfe5e4b4d0b2782618b09e2603e65

Marlin/Configuration.h
@@ -10,7 +10,7 @@
 //#define TazDualZ
 
 /************** Uncomment a Tool Head Option From Below *********************/
-//#define LULZBOT_UNIVERSAL_TOOLHEAD
+#define LULZBOT_UNIVERSAL_TOOLHEAD
 //#define TOOLHEAD_SL_SE_HE
 //#define TOOLHEAD_HS_HSPLUS
 //#define TOOLHEAD_H175
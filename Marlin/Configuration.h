0a86e8129a837958d852647b80ca824e01e9a9b3
Brian Kahl <bkahl.lulzbot.com>
2021-09-15 08:07:23 -0500

    Fixing redundant if statement

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1581,10 +1581,10 @@
 #elif ENABLED(Workhorse)
   #define PROBE_SAFE_POINT { -10, -10 }
 #elif ANY(TAZPro, TAZProXT)
-    #if DEFINED(TOOLHEAD_Quiver_DualExtruder)
-        #define PROBE_SAFE_POINT { -6, -9 } //safe probe point for dual extruder on Pro/XT
-    #else
-        #define PROBE_SAFE_POINT { -1, -9 } //safe probe point for single extruder toolheads on Pro/XT
+  #if DEFINED(TOOLHEAD_Quiver_DualExtruder)
+    #define PROBE_SAFE_POINT { -6, -9 } //safe probe point for dual extruder on Pro/XT
+  #else
+    #define PROBE_SAFE_POINT { -1, -9 } //safe probe point for single extruder toolheads on Pro/XT
 #endif
 
 // X and Y axis travel speed (mm/min) between probes

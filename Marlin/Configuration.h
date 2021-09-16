b57a369fbcd4d4819f660601bf80c4c824a9b4d6
Brian Kahl <bkahl.lulzbot.com>
2021-09-16 07:31:08 -0500

    loading PLA at a higher temp of 200

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2428,7 +2428,7 @@
 // Preheat Constants - Up to 5 are supported without changes
 //
 #define PREHEAT_1_LABEL       "PLA"
-#define PREHEAT_1_TEMP_HOTEND 190
+#define PREHEAT_1_TEMP_HOTEND 200
 #define PREHEAT_1_TEMP_BED     65
 #define PREHEAT_1_TEMP_CHAMBER 35
 #define PREHEAT_1_FAN_SPEED     0 // Value from 0 to 255

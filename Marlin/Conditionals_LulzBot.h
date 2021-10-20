6c3ee9663f43039ece1c36784f81fd20ce3594af
Brian Kahl <bkahl@lulzbot.com>
2021-10-20 09:06:22 -0500

    updating version number for Bilinear leveling

--- Marlin/Conditionals_LulzBot.h
+++ Marlin/Conditionals_LulzBot.h
@@ -73,7 +73,7 @@
  *
  */
 
-#define LULZBOT_FW_VERSION ".144.5" // Change this with each update
+#define LULZBOT_FW_VERSION ".144.6" // Change this with each update
 
 #if ( \
     !defined(LULZBOT_Gladiola_Mini) && \

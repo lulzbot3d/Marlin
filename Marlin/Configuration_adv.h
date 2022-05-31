305d9c89c3542b7afc3fe24f533c3f3c4d7a9142
bmkahl <bkahl@lulzbot.com>
2022-05-31 10:03:47 -0500

    correcting offsets and allowing connection to the calibration cube

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -765,8 +765,13 @@
 
   // Define pin which is read during calibration
   #ifndef CALIBRATION_PIN
-    #define CALIBRATION_PIN 31 // Override in pins.h or set to -1 to use your Z endstop
-    #define CALIBRATION_PIN_INVERTING false // Set to true to invert the pin
+    #if defined(LULZBOT_BLTouch)
+      #define CALIBRATION_PIN 31 // Override in pins.h or set to -1 to use your Z endstop
+      #define CALIBRATION_PIN_INVERTING true // Set to true to invert the pin
+    #else
+      #define CALIBRATION_PIN -1 // Override in pins.h or set to -1 to use your Z endstop
+      #define CALIBRATION_PIN_INVERTING false // Set to true to invert the pin
+    #endif
     //#define CALIBRATION_PIN_PULLDOWN
     #define CALIBRATION_PIN_PULLUP
   #endif

e978f08d25d46bcf08ea72a7c008b2955744e0dc
bmkahl <bkahl@lulzbot.com>
2022-10-13 16:52:34 -0500

    updating version number

--- Marlin/Conditionals_LulzBot.h
+++ Marlin/Conditionals_LulzBot.h
@@ -73,7 +73,7 @@
  *
  */
 
-#define LULZBOT_FW_VERSION ".144.7" // Change this with each update
+#define LULZBOT_FW_VERSION ".144.8" // Change this with each update
 
 #if ( \
     !defined(LULZBOT_Gladiola_Mini) && \

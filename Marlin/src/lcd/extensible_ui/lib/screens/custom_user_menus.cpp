20330f27361e7794d49a54c1dd0c1a6c36c71028
bkahl <bkahl@lulzbot.com>
2023-08-21 08:17:38 -0500

    simplify math to only use one column

--- Marlin/src/lcd/extensible_ui/lib/screens/custom_user_menus.cpp
+++ Marlin/src/lcd/extensible_ui/lib/screens/custom_user_menus.cpp
@@ -42,35 +42,19 @@ void CustomUserMenus::onRedraw(draw_mode_t what) {
        .cmd(CLEAR(true, true, true));
   }
 
-   //#if HAS_USER_ITEM(16, 17, 18, 19, 20)
-   //  #define _MORE_THAN_FIFTEEN 1
-   //#else
-    #define _MORE_THAN_FIFTEEN 0
-   //#endif
-   //#if _MORE_THAN_FIFTEEN || HAS_USER_ITEM(11, 12, 13, 14, 15)
-   //  #define _MORE_THAN_TEN 1
-   //#else
-    #define _MORE_THAN_TEN 0
-   //#endif
-
   #if ENABLED(TOUCH_UI_PORTRAIT)
     #define GRID_ROWS 11
-    #define GRID_COLS (1 + _MORE_THAN_TEN)
+    #define GRID_COLS 1
     #define USER_ITEM_POS(N) BTN_POS((1+((N-1)/10)), ((N-1) % 10 + 1)), BTN_SIZE(1,1)
     #define BACK_POS         BTN_POS(1,11), BTN_SIZE(1,1)
   #else
-    // #if _MORE_THAN_TEN || HAS_USER_ITEM(6, 7, 8, 9, 10)
-       #define _MORE_THAN_FIVE 1
-    // #else
-    //  #define _MORE_THAN_FIVE 0
-    //#endif
     #if defined(TOOLHEAD_Legacy_Universal)
       #define GRID_ROWS 9
     #else
       #define GRID_ROWS 8
     #endif
     #define GRID_COLS 1
-    #define USER_ITEM_POS(N) BTN_POS((1+((N-1)/7)), ((N-1) % 7 + 1)), BTN_SIZE(1,1)
+    #define USER_ITEM_POS(N) BTN_POS(1, N), BTN_SIZE(GRID_COLS,1)
     #define BACK_POS         BTN_POS(1,GRID_ROWS), BTN_SIZE(GRID_COLS,1)
   #endif
 
@@ -80,36 +64,36 @@ void CustomUserMenus::onRedraw(draw_mode_t what) {
        .font(Theme::font_medium)
        #if defined(MAIN_MENU_ITEM_1_DESC)
         //_USER_ITEM(1)
-        .tag(_ITEM_TAG(11)).button(USER_ITEM_POS(1), MAIN_MENU_ITEM_1_DESC) 
+        .tag(_ITEM_TAG(11)).button(USER_ITEM_POS(1), MAIN_MENU_ITEM_1_DESC)
        #endif
       .colors(normal_btn)
        #if defined(MAIN_MENU_ITEM_2_DESC)
         //_USER_ITEM(2)
-        .tag(_ITEM_TAG(12)).button(USER_ITEM_POS(2), MAIN_MENU_ITEM_2_DESC) 
+        .tag(_ITEM_TAG(12)).button(USER_ITEM_POS(2), MAIN_MENU_ITEM_2_DESC)
        #endif
        #if defined(MAIN_MENU_ITEM_3_DESC)
         //_USER_ITEM(3)
-        .tag(_ITEM_TAG(13)).button(USER_ITEM_POS(3), MAIN_MENU_ITEM_3_DESC) 
+        .tag(_ITEM_TAG(13)).button(USER_ITEM_POS(3), MAIN_MENU_ITEM_3_DESC)
        #endif
        #if defined(MAIN_MENU_ITEM_4_DESC)
         //_USER_ITEM(4)
-        .tag(_ITEM_TAG(14)).button(USER_ITEM_POS(4), MAIN_MENU_ITEM_4_DESC) 
+        .tag(_ITEM_TAG(14)).button(USER_ITEM_POS(4), MAIN_MENU_ITEM_4_DESC)
        #endif
        #if defined(MAIN_MENU_ITEM_5_DESC)
         //_USER_ITEM(5)
-        .tag(_ITEM_TAG(15)).button(USER_ITEM_POS(5), MAIN_MENU_ITEM_5_DESC) 
+        .tag(_ITEM_TAG(15)).button(USER_ITEM_POS(5), MAIN_MENU_ITEM_5_DESC)
        #endif
        #if defined(MAIN_MENU_ITEM_6_DESC)
         //_USER_ITEM(6)
-        .tag(_ITEM_TAG(16)).button(USER_ITEM_POS(6), MAIN_MENU_ITEM_6_DESC) 
+        .tag(_ITEM_TAG(16)).button(USER_ITEM_POS(6), MAIN_MENU_ITEM_6_DESC)
        #endif
        #if defined(MAIN_MENU_ITEM_7_DESC)
         //_USER_ITEM(7)
-        .tag(_ITEM_TAG(17)).button(USER_ITEM_POS(7), MAIN_MENU_ITEM_7_DESC) 
+        .tag(_ITEM_TAG(17)).button(USER_ITEM_POS(7), MAIN_MENU_ITEM_7_DESC)
        #endif
        #if defined(MAIN_MENU_ITEM_8_DESC)
         //_USER_ITEM(8)
-        .tag(_ITEM_TAG(18)).button(BTN_POS(1,8), BTN_SIZE(GRID_COLS,1), MAIN_MENU_ITEM_8_DESC) 
+        .tag(_ITEM_TAG(18)).button(BTN_POS(1,8), BTN_SIZE(GRID_COLS,1), MAIN_MENU_ITEM_8_DESC)
        #endif
       .colors(action_btn)
       .tag(1).button(BACK_POS, F(MSG_BACK));

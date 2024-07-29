5172ee67813a21d57608e5cfdcb8adf699aa9c5f
bmkahl <bkahl@lulzbot.com>
2024-07-29 14:04:28 -0500

    🎨 Update to messages on Calibration screen

--- Marlin/src/lcd/extui/ftdi_eve_touch_ui/generic/z_offset_screen.cpp
+++ Marlin/src/lcd/extui/ftdi_eve_touch_ui/generic/z_offset_screen.cpp
@@ -58,11 +58,6 @@ void ZOffsetScreen::onRedraw(draw_mode_t what) {
   w.color(z_axis).adjuster(4, GET_TEXT_F(MSG_ZOFFSET), getZOffset_mm());
   w.increments();
   //w.button(2, GET_TEXT_F(MSG_PROBE_WIZARD), !isPrinting() && !wizardRunning());
-  if (what & FOREGROUND) {
-    cmd.colors(normal_btn)
-       .font(font_medium)
-       .tag(6).colors(action_btn).button(BTN_POS(1,GRID_ROWS), BTN_SIZE(GRID_COLS,1), GET_TEXT_F(MSG_BUTTON_DONE));
-  }
 }
 
 void ZOffsetScreen::move(float mm, int16_t steps) {
@@ -110,7 +105,6 @@ bool ZOffsetScreen::onTouchHeld(uint8_t tag) {
     case 2: runWizard(); break;
     case 4: UI_DECREMENT(ZOffset_mm); move(-increment, -steps); break;
     case 5: UI_INCREMENT(ZOffset_mm); move( increment,  steps); break;
-    case 6: GOTO_SCREEN(SaveSettingsDialogBox); break;
     default:
       return false;
   }

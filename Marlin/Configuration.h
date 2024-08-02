df0802b5b01237864778e3d25ca2f268132ac5d9
bmkahl <bkahl@lulzbot.com>
2024-08-02 16:33:47 -0500

    🔨Fix Pro2 travel

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2672,7 +2672,7 @@
       #define LULZBOT_X_MIN_POS 10 // <-- changed
       #define LULZBOT_Y_MIN_POS -24 // <-- changed
       #define LULZBOT_X_MAX_POS 288 // <-- changed
-      #define LULZBOT_Y_MAX_POS 330 // <-- changed
+      #define LULZBOT_Y_MAX_POS 339 // <-- changed
       #define LULZBOT_Z_MIN_POS -9 // <-- changed
       #define LULZBOT_Z_MAX_POS 301 // <-- changed
     #endif

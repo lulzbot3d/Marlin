aab9e55e46ff4fccde6956edc9982f71a3bb006d
GrimQuagle <sfjelstad@lulzbot.com>
2025-04-25 13:50:50 -0500

    2.1.3.0.46 Release

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -99,7 +99,7 @@
 // Author info of this build printed to the host during boot and M115
 #define STRING_CONFIG_H_AUTHOR "LulzBot" // Who made the changes.
 #define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
-#define LULZBOT_FW_VERSION "2.1.3.0.46-Dev.2"
+#define LULZBOT_FW_VERSION "2.1.3.0.46"
 #define CAPABILITIES_REPORT
 #define EXTENDED_CAPABILITIES_REPORT
 
